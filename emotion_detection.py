7d30b5421309f18d052f61a489e0ca5f68b947c0
edgardov29 <edgardovelasco29@gmail.com>
2026-06-20 01:44:49 -0400

    Tarea 3 lista

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,12 +1,17 @@
 import requests
+import json
 
 url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
 headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
 
-
 def emotion_detector(text_to_analyze):
     input_json = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, headers=headers, json=input_json)
-    return response.text
+    result = response.json()  # ahora result es el diccionario
+    emotions = result["emotionPredictions"][0]["emotion"]
 
-#emotion_detector("Me encantar esta nueva tecnologia")
+    # calcular emoción dominante
+    dominant_emotion = max(emotions, key=emotions.get)
+    emotions["dominant_emotion"] = dominant_emotion
+
+    return emotions
